352e2e74e8e379303567cf163735a4d5fbb26a6a
Alan M. Mendes <alanmazeto.m@gmail.com>
2024-09-02 16:39:08 -0300

    commit: added new files to the vue project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+  <div>
+   
+  
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+.counter {
+  text-align: center;
+  margin-top: 20px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
---
+++ uno.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2
+    })
+  ],
+  rules: [],
+  shortcuts: []
+})
